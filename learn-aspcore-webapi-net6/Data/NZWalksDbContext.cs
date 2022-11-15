bcc87818445a2aa2707c9808f81bdbe0bb001e60
Derek <derek@smallboat.com>
2022-11-15 15:08:26 +0800

    Add project files.

---
+++ learn-aspcore-webapi-net6/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using learn_aspcore_webapi_net6.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace learn_aspcore_webapi_net6.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ learn-aspcore-webapi-net6/Models/Domains/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace learn_aspcore_webapi_net6.Models.Domains
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
